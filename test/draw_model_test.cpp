f78198c90c4dd7d0fa2df05da977c530ce118b7f
kkaneko <45529381+kkaneko-42@users.noreply.github.com>
2023-10-19 01:56:20 +0900

    add tiny obj loader

---
+++ test/draw_model_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "kk_renderer/kk_renderer.h"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tinyobjloader/tiny_obj_loader.h>
+#ifndef TEST_RESOURCE_DIR
+#define TEST_RESOURCE_DIR "./resources"
+#endif
+
+TEST(DrawModelTest, ModelCreation) {
+
+}
